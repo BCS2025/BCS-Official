a4f4f2ce15a5199393fcb5663ff7d60a52744a65
BCS2025 <bc2024space@gmail.com>
2026-01-28 20:29:43 +0800

    Fix Blank problem

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/BCS-Official">
       <App />
     </BrowserRouter>
   </StrictMode>,
